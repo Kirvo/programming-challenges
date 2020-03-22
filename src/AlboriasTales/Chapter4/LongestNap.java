35a594b0197b93b6f74337a7e54c5c66a15cf5da
Kirvo <kirvo.software@gmail.com>
2020-03-22 20:46:58 +0100

    LongestNap is working! :D

--- src/AlboriasTales/Chapter4/LongestNap.java
+++ src/AlboriasTales/Chapter4/LongestNap.java
@@ -18,42 +18,36 @@ public class LongestNap {
             // Obtenemos listado de registros
             while (--numOfLines >= 0) {
                 String appointment = scan.nextLine().trim();
-                String[] times = appointment.split(" ", 3);
 
-                dates.put(sdf.parse(times[0]).getTime(), sdf.parse(times[1]).getTime());
+                String[] times = appointment.split(" ", 3);
+                long key = sdf.parse(times[0]).getTime(), value = sdf.parse(times[1]).getTime();
+
+                // Si tenemos dos citas que empiezan a la misma hora, tiene preferencia la que dure más
+                if(dates.containsKey(key)) {
+                    value = Math.max(value, dates.get(key));
+                }
+
+                dates.put(key, value);
             }
 
-            dates.forEach((key, value) -> {
-                int hoursStart = (int) ((key / 60000) / 60) + 1, minutesStart = (int) ((key / 60000) % 60);
-                int hoursEnd = (int) ((value / 60000) / 60) + 1, minutesEnd = (int) ((value / 60000) % 60);
+            dates.put(sdf.parse("18:00").getTime(), sdf.parse("18:00").getTime());
 
-                System.out.println(hoursStart + ":" + minutesStart + "  " + hoursEnd + ":" + minutesEnd);
-            });
-
-            long nap = 1; int cont = 0;
-            Date startTime = sdf.parse("10:00"), time = startTime;
+            long nap = 0, napStartTime = sdf.parse("10:00").getTime(), maxValue = napStartTime;
 
             // Sacamos el resultado
-            for(long A : dates.keySet()) {
-                long calc = A - startTime.getTime();
+            for(long key : dates.keySet()) {
+                if(maxValue <= key) {
+                    long tempNap = key - maxValue, lastNap = nap;
+                    nap = Math.max(tempNap, lastNap);
 
-                if(A > 0 && Math.max(calc, nap) == calc) {
-                    nap = calc;
-                    time = startTime;
+                    // Si el valor máximo no ha cambiado, predomina el primer valor cuando guardamos la hora de inicio de la siesta
+                    if(nap != lastNap) { napStartTime = maxValue; }
                 }
 
-                startTime = new Date(dates.get(A));
-
-                if(++cont == dates.keySet().size()) {
-                    calc = sdf.parse("18:00").getTime() - startTime.getTime();
-                    if(Math.max(calc, nap) == calc) {
-                        nap = calc;
-                        time = startTime;
-                    }
-                }
+                maxValue = Math.max(maxValue, dates.get(key));
             }
 
-            responses.add(getResponse(dayCounter++, time.getTime() / 60000, nap / 60000));
+            responses.add(getResponse(dayCounter++, napStartTime / 60000, nap / 60000));
         }
 
         responses.forEach(System.out::println);
@@ -72,6 +66,15 @@ public class LongestNap {
 
         if(hours > 0) { res += hours + " hours and "; } res += minutes + " minutes.";
 
+        return res;
+    }
+
+    private static String getDateFrom(long time) {
+        if(time > 60000) time = time / 60000;
+
+        String res = "";
+        int hours = (int) (time / 60) + 1; if(hours <= 9) { res += "0"; } res += hours + ":";
+        int minutes = (int) (time % 60); if(minutes <= 9) { res += "0"; } res += minutes;
         return res;
     }
 }
